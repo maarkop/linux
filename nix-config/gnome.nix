e4e66abf3c03ac7bd2f1a6dab3d5c71004d4e439
guhhsle <guhhsle@proton.me>
2025-03-08 19:42:08 +0100

    Removed bloat

--- nix-config/gnome.nix
+++ nix-config/gnome.nix
@@ -5,30 +5,4 @@
     tiling-shell
   ];
 
-  programs.dconf = {
-    enable = true;
-    profiles = {
-      user = {
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = with pkgs.gnomeExtensions; [
-            blur-my-shell.extensionUuid
-            tiling-shell.extensionUuid
-            system-monitor.extensionUuid
-          ];
-          disabled-extensions = [
-            "dash-to-dock@micxgx.gmail.com"
-            "window-list@gnome-shell-extensions.gcampax.github.com"
-            "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-            "light-style@gnome-shell-extensions.gcampax.github.com"
-            "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
-            "apps-menu@gnome-shell-extensions.gcampax.github.com"
-            "emoji-copy@felipeftn"
-            "native-window-placement@gnome-shell-extensions.gcampax.github.com"
-            "status-icons@gnome-shell-extensions.gcampax.github.com"
-          ];
-        };
-      };
-    };
-  };
 }
