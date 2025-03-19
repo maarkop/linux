1b90116703174342d42a471cf420fe0180d7b0bd
guhhsle <guhhsle@proton.me>
2025-03-19 11:42:04 +0100

    Raw flutter

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,6 @@ function fish_greeting
     echo Hello (set_color yellow)(date +%T)(set_color normal) :P(set_color green)
 end
 
-function flutter
-		steam-run flutter $argv
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
