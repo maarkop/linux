af3f4e3f87cb84e11eaeaf391f427266f8d89222
guhhsle <guhhsle@proton.me>
2025-05-27 21:27:33 +0200

    removed fish

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-function fish_greeting
-    echo Hello (set_color yellow)(date +%T)(set_color normal) :P(set_color green)
-end
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
