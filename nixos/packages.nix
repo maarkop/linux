ece6cba2c058a7e6908f9b8185518e0349d42953
markopetrovic <markopetrovic3939@gmail.com>
2025-06-27 17:28:57 +0200

    Removed gparted & slop

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -14,7 +14,7 @@
   
   environment.systemPackages = with pkgs;[
     gcc fzf bombadillo vlc deluge eartag 
-		retroarchFull kooha lazygit tuba
+		retroarchFull kooha lazygit
     ffmpeg imagemagick optipng jpegoptim
 	];
 
