02acea5258db9d73546ddc0ffc9f7c5cf7443dd3
markopetrovic <markopetrovic3939@gmail.com>
2025-07-09 18:56:51 +0200

    I2P

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -5,7 +5,23 @@
 	services.printing.enable = false;
   programs.firefox.enable = true;
   programs.git.enable = true;
+  
   services.i2p.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 
+    4444 7657 #I2P 
+    22000     #Syncthing 
+  ];
+
+  networking.firewall.allowedUDPPorts = [ 
+    21027     #Syncthing 
+    42410     #I2P Custom
+  ];
+
+  environment.etc."i2p/router.config".text = ''
+    i2np.udp.internalPort=42410
+    i2np.udp.port=42410
+  '';
 
   programs.steam = {
     enable = true;
