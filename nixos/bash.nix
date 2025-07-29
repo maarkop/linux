001aef3c13ab07703eb2b498a3f7f9b670fabef5
markopetrovic <markopetrovic3939@gmail.com>
2025-07-29 15:05:24 +0200

    Minimal bash

--- nixos/bash.nix
+++ nixos/bash.nix
@@ -1,9 +1,6 @@
 { ... } : {
   programs.bash.promptInit = ''
-    PROMPT_COLOR="1;31m"
-    ((UID)) && PROMPT_COLOR="1;32m"
-    PS1="\[\033[$PROMPT_COLOR\][ \w ] \$\[\033[0m\] "
-    
+    PS1="[ \w ] \$\[\033[0m\] "
     eval "$(fzf --bash)"
   '';
 }
