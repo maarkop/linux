f37a8e949461da85e08db7026ba025502fbf4f4d
markopetrovic <markopetrovic3939@gmail.com>
2025-07-29 14:15:11 +0200

    Bash

--- nixos/bash.nix
+++ nixos/bash.nix
@@ -2,7 +2,7 @@
   programs.bash.promptInit = ''
     PROMPT_COLOR="1;31m"
     ((UID)) && PROMPT_COLOR="1;32m"
-    PS1="\n\[\033[$PROMPT_COLOR\][\w]\\$\[\033[0m\] "
+    PS1="\[\033[$PROMPT_COLOR\][\w]\\$\[\033[0m\] "
     
     eval "$(fzf --bash)"
   '';
